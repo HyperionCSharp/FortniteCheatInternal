60e3cda45f5b6fcb14bfcf4937026b038dcc2c29
HyperionCSharp <94261284+HyperionCSharp@users.noreply.github.com>
2023-05-31 17:01:58 -0500

    added header file includes | Base: 1 code

---
+++ FortniteCheat/includes.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#define _CRT_SECURE_NO_WARNINGS
+
+// Windows Header Files
+#include <Windows.h>
+#include <iostream>
+#include <string>
+#include <math.h>
+#include <cstdint>
+#include <stdlib.h>
+// ImGui Header Files
+#include <imgui.h>
+#include <imgui_impl_dx11.h>
+#include <imgui_internal.h>
+// Minhook Lib File
+#include <MinHook.h>
+#pragma comment(lib, "minhook.lib")
